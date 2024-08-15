1cb3931f09931dc880d69d05f94fa23389a532fe
extremq <45830561+extremq@users.noreply.github.com>
2024-08-15 12:46:51 +0300

    remove stuff

--- guessing_game/target/debug/deps/guessing_game-65e53c6dcbfd378c.d
+++
@@ -1,5 +0,0 @@
-/home/mr/Projects/rustbook/guessing_game/target/debug/deps/libguessing_game-65e53c6dcbfd378c.rmeta: src/main.rs
-
-/home/mr/Projects/rustbook/guessing_game/target/debug/deps/guessing_game-65e53c6dcbfd378c.d: src/main.rs
-
-src/main.rs:
