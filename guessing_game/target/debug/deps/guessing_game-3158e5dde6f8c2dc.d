b4406fe6f91982fd669c14c31f85a0981b3e508f
extremq <contact@suntdoar.eu>
2024-08-15 12:42:57 +0300

    init

---
+++ guessing_game/target/debug/deps/guessing_game-3158e5dde6f8c2dc.d
@@ -0,0 +1,5 @@
+/home/mr/Projects/rustbook/guessing_game/target/debug/deps/libguessing_game-3158e5dde6f8c2dc.rmeta: src/main.rs
+
+/home/mr/Projects/rustbook/guessing_game/target/debug/deps/guessing_game-3158e5dde6f8c2dc.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-65e53c6dcbfd378c.d
@@ -0,0 +1,5 @@
+/home/mr/Projects/rustbook/guessing_game/target/debug/deps/libguessing_game-65e53c6dcbfd378c.rmeta: src/main.rs
+
+/home/mr/Projects/rustbook/guessing_game/target/debug/deps/guessing_game-65e53c6dcbfd378c.d: src/main.rs
+
+src/main.rs:
